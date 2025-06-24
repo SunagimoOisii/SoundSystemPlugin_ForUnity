d29749a305186360687e29cc8350319365bf961b
Fukada_Ryuto <ryuryu57057@gmail.com>
2025-06-25 01:05:24 +0900

    refactor: #if の順序を変更

--- SoundSystemPlugin_ForUnity_Project/source/SoundSystem.cs
+++ SoundSystemPlugin_ForUnity_Project/source/SoundSystem.cs
@@ -27,6 +27,7 @@ namespace SoundSystem
 
         private readonly ISoundCache cache;
         private CancellationTokenSource autoEvictCTS;
+
         private bool autoDisposeOnSceneChange;
 
         public SoundSystem(ISoundLoader loader, ISoundCache cache, IAudioSourcePool pool,
@@ -243,31 +244,6 @@ namespace SoundSystem
         }
         #endregion
 
-        #region AutoDispose
-
-        public void EnableAutoDisposeOnSceneChange()
-        {
-            if (autoDisposeOnSceneChange) return;
-
-            SceneManager.activeSceneChanged += OnActiveSceneChanged;
-            autoDisposeOnSceneChange = true;
-        }
-
-        public void DisableAutoDisposeOnSceneChange()
-        {
-            if (autoDisposeOnSceneChange == false) return;
-
-            SceneManager.activeSceneChanged -= OnActiveSceneChanged;
-            autoDisposeOnSceneChange = false;
-        }
-
-        private void OnActiveSceneChanged(Scene current, Scene next)
-        {
-            Dispose();
-        }
-
-        #endregion
-
         #region AutoEvict
 
         public void StartAutoEvict(float interval)
@@ -303,9 +279,35 @@ namespace SoundSystem
 
         #endregion
 
+        #region AutoDispose
+
+        public void EnableAutoDisposeOnSceneChange()
+        {
+            if (autoDisposeOnSceneChange) return;
+
+            SceneManager.activeSceneChanged += OnActiveSceneChanged;
+            autoDisposeOnSceneChange = true;
+        }
+
+        public void DisableAutoDisposeOnSceneChange()
+        {
+            if (autoDisposeOnSceneChange == false) return;
+
+            SceneManager.activeSceneChanged -= OnActiveSceneChanged;
+            autoDisposeOnSceneChange = false;
+        }
+
+        private void OnActiveSceneChanged(Scene current, Scene next)
+        {
+            Dispose();
+        }
+
+        #endregion
+
         public void Dispose()
         {
             DisableAutoDisposeOnSceneChange();
+
             StopAutoEvict();
             bgm.Dispose();
             se.Dispose();
